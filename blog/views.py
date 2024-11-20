8c9557d4ac4998d3effa9e5d15e2b55c341c3820
pavlo <frontbastard@gmail.com>
2024-11-21 00:07:03 +0100

    Implement SEO-friendly URLs for Post

--- blog/views.py
+++ blog/views.py
@@ -9,19 +9,22 @@ def post_list(request):
     return render(
         request,
         "blog/post/list.html",
-        {"posts": posts}
+        {"posts": posts},
     )
 
 
-def post_detail(request, id):
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(
         Post,
-        id=id,
-        status=Post.Status.PUBLISHED
+        status=Post.Status.PUBLISHED,
+        slug=post,
+        publish__year=year,
+        publish__month=month,
+        publish__day=day,
     )
 
     return render(
         request,
         "blog/post/detail.html",
-        {"post": post}
+        {"post": post},
     )
