cbf3b1eb0f4a16fca8db1f9e918eb9f1dfdeaadc
pavlo <frontbastard@gmail.com>
2024-11-22 11:52:30 +0100

    Add a Share post functionality

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.mail import send_mail
 from django.views.generic import ListView
 
 from blog.forms import EmailPostForm
@@ -35,12 +36,31 @@ def post_share(request, post_id):
         id=post_id,
         status=Post.Status.PUBLISHED,
     )
+    sent = False
 
     if request.method == "POST":
         form = EmailPostForm(request.POST)
 
         if form.is_valid():
             cd = form.cleaned_data
+            post_url = request.build_absolute_uri(
+                post.get_absolute_url()
+            )
+            subject = (
+                f"{cd['name']} ({cd['email']}) "
+                f"recommends you read {post.title}"
+            )
+            message = (
+                f"Read {post.title} at {post_url}\n\n"
+                f"{cd['name']}\'s comments: {cd['comments']}"
+            )
+            send_mail(
+                subject=subject,
+                message=message,
+                from_email=None,
+                recipient_list=[cd["to"]]
+            )
+            sent = True
     else:
         form = EmailPostForm()
 
@@ -50,5 +70,6 @@ def post_share(request, post_id):
         {
             "post": post,
             "form": form,
+            "sent": sent,
         }
     )
