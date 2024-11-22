21459a6bcd29757a09beae3d06d563740e439324
pavlo <frontbastard@gmail.com>
2024-11-22 10:28:07 +0100

    Create form, view and template for sending emails

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 
+from blog.forms import EmailPostForm
 from blog.models import Post
 
 
@@ -25,4 +26,29 @@ def post_detail(request, year, month, day, post):
         request,
         "blog/post/detail.html",
         {"post": post},
+    )
+
+
+def post_share(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED,
+    )
+
+    if request.method == "POST":
+        form = EmailPostForm(request.POST)
+
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+
+    return render(
+        request,
+        "blog/post/share.html",
+        {
+            "post": post,
+            "form": form,
+        }
     )
