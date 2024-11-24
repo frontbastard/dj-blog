bafcdeed73eb9c386fca044d3f1d071b10c3f2da
pavlo <frontbastard@gmail.com>
2024-11-24 01:31:39 +0100

    Add the comment system

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.mail import send_mail
 from django.views.generic import ListView
+from django.views.decorators.http import require_POST
 
-from blog.forms import EmailPostForm
+from blog.forms import EmailPostForm, CommentForm
 from blog.models import Post
 
 
@@ -22,11 +23,17 @@ def post_detail(request, year, month, day, post):
         publish__day=day,
         slug=post,
     )
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
 
     return render(
         request,
         "blog/post/detail.html",
-        {"post": post},
+        {
+            "post": post,
+            "comments": comments,
+            "form": form,
+        },
     )
 
 
@@ -71,5 +78,31 @@ def post_share(request, post_id):
             "post": post,
             "form": form,
             "sent": sent,
+        }
+    )
+
+
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.Status.PUBLISHED,
+    )
+    comment = None
+    form = CommentForm(data=request.POST)
+
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+
+    return render(
+        request,
+        "blog/post/comment.html",
+        {
+            "post": post,
+            "form": form,
+            "comment": comment,
         }
     )
