00a0626b51aa23d52994463b02bc93702efeecb3
pavlo <frontbastard@gmail.com>
2024-11-26 22:33:09 +0100

    Add the full-text-search to the blog

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,16 @@
+from django.contrib.postgres.search import (
+    SearchVector,
+    SearchQuery,
+    SearchRank, TrigramSimilarity,
+)
+from django.core.mail import send_mail
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
-from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 
-from blog.forms import EmailPostForm, CommentForm
+from blog.forms import EmailPostForm, CommentForm, SearchForm
 from blog.models import Post
 
 
@@ -141,5 +146,38 @@ def post_comment(request, post_id):
             "post": post,
             "form": form,
             "comment": comment,
+        }
+    )
+
+
+def post_search(request):
+    form = SearchForm
+    query = None
+    results = []
+
+    if "query" in request.GET:
+        form = SearchForm(request.GET)
+
+        if form.is_valid():
+            query = form.cleaned_data["query"]
+            search_vector = SearchVector(
+                "title", weight="A"
+            ) + SearchVector("body", weight="B")
+            search_query = SearchQuery(query)
+            results = (
+                Post.published.annotate(
+                    similarity=TrigramSimilarity("title", query),
+                )
+                .filter(similarity__gt=0.1)
+                .order_by("-similarity")
+            )
+
+    return render(
+        request,
+        "blog/post/search.html",
+        {
+            "form": form,
+            "query": query,
+            "results": results,
         }
     )
